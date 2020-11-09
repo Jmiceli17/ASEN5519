815850dbe2adfe164580e06ae12f443975af99e6
Joe Miceli <joe.miceli8@gmail.com>
2020-11-08 19:40:55 -0700

    debugging

---
+++ Homework4/ProbabilisticRoadmap/plot_prm.py
@@ -0,0 +1,40 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+if __name__ == "__main__":
+
+	if (len(sys.argv) < 2):
+		print("ERROR")
+		print("USAGE: python plot_prm.py <Path To PRM.csv>")
+	
+	else:
+		prm_path = sys.argv[1]
+
+		# ==== GET DATA FROM LOG FILES ====
+
+		data = np.genfromtxt(prm_path, delimiter=",", names=True)
+
+		#data = np.genfromtxt("PRM.csv", delimiter=",", names=True)
+
+
+		# ==== PLOT PATH ==== 
+		fig1 = plt.figure(1)
+		axes1 = plt.gca()
+
+		plt.plot(data['X'], data['Y'], 'o')
+
+		# Labels
+		#axes1.set_title('Path Taken by Robot in 2 Dimensional Space \n(Workspace 1 from Exercise 7 of Homework 1)')
+		axes1.set_xlabel('X position (m)')
+		axes1.set_ylabel('Y position (m)')
+
+		# Range for w-space
+		#axes1.set_xlim([0,11])
+		#axes1.set_ylim([-2,2])
+
+		axes1.grid(color='y', linestyle='--')
+
+		plt.show()
